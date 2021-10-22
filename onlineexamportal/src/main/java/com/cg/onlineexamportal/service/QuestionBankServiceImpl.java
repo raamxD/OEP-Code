8efd417f858a5fbef5cf065f983361c06eb5becf
raamxD <ram22ksh3@gmail.com>
2021-10-22 08:16:22 +0530

    initial commit

--- onlineexamportal/src/main/java/com/cg/onlineexamportal/service/QuestionBankServiceImpl.java
+++ onlineexamportal/src/main/java/com/cg/onlineexamportal/service/QuestionBankServiceImpl.java
@@ -16,23 +16,43 @@ public class QuestionBankServiceImpl implements QuestionBankService{
 	@Autowired
 	private QuestionBankRepository questionBankRepository;
 	
+	/**
+	 *	Method Description : Get list of all question banks
+	 *	@param 
+	 *  @return ResponseEntity<List<QuestionBank>>
+	 */
 	@Override
 	public ResponseEntity<List<QuestionBank>> getQuestionBanks(){
 		List<QuestionBank> questionBanks = questionBankRepository.findAll();
 		return ResponseEntity.ok().body(questionBanks);
 	}
 
+	/**
+	 *	Method Description : Get a question bank by id
+	 *	@param Long questionBankId
+	 *  @return ResponseEntity<QuestionBank>
+	 */
 	@Override
 	public ResponseEntity<QuestionBank> getQuestionBankById(Long questionBankId) throws QuestionBankNotFoundException {
 		QuestionBank questionBank = questionBankRepository.findById(questionBankId).orElseThrow(() -> new QuestionBankNotFoundException("Question Bank ID :: " + questionBankId + " Not Found"));
 		return ResponseEntity.ok().body(questionBank);
 	}
 
+	/**
+	 *	Method Description : Create a question bank
+	 *	@param Long questionBankId
+	 *  @return QuestionBank
+	 */
 	@Override
 	public QuestionBank addQuestionBank(QuestionBank questionBank) {
 		return questionBankRepository.save(questionBank);
 	}
 
+	/**
+	 *	Method Description : Update a question bank by id
+	 *	@param Long questionBankId, QuestionBank questionBank
+	 *  @return ResponseEntity<QuestionBank>
+	 */
 	@Override
 	public ResponseEntity<QuestionBank> updateQuestionBankById(Long questionBankId, QuestionBank questionBank) throws QuestionBankNotFoundException {
 		QuestionBank newQuestionBank = questionBankRepository.findById(questionBankId).orElseThrow(() -> new QuestionBankNotFoundException("Question Bank ID :: " + questionBankId + " Not Found"));
@@ -43,6 +63,11 @@ public class QuestionBankServiceImpl implements QuestionBankService{
 		return ResponseEntity.ok().body(updatedQuestionBank);
 	}
 
+	/**
+	 *	Method Description : Delete a question bank by id
+	 *	@param Long questionBankId
+	 *  @return ResponseEntity<QuestionBank>
+	 */
 	@Override
 	public ResponseEntity<QuestionBank> deleteQuestionBankById(Long questionBankId) throws QuestionBankNotFoundException {
 		QuestionBank questionBank = questionBankRepository.findById(questionBankId).orElseThrow(() -> new QuestionBankNotFoundException("Question Bank ID :: " + questionBankId + " Not Found"));
--- onlineexamportal/src/main/java/com/cg/onlineexamportal/service/QuestionServiceImpl.java
+++ onlineexamportal/src/main/java/com/cg/onlineexamportal/service/QuestionServiceImpl.java
@@ -16,27 +16,46 @@ public class QuestionServiceImpl implements QuestionService{
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+	/**
+	 *	Method Description : Get list of all questions
+	 *	@param 
+	 *  @return ResponseEntity<List<Question>>
+	 */
 	@Override
 	public ResponseEntity<List<Question>> getQuestions(){
 		List<Question> questions = questionRepository.findAll();
 		return ResponseEntity.ok().body(questions);
 	}
 
+	/**
+	 *	Method Description : Get a question by id
+	 *	@param Long questionId
+	 *  @return ResponseEntity<Question>
+	 */
 	@Override
 	public ResponseEntity<Question> getQuestionById(Long questionId) throws QuestionNotFoundException {
 		Question question = questionRepository.findById(questionId).orElseThrow(() -> new QuestionNotFoundException("Question ID :: " + questionId + " Not Found"));
 		return ResponseEntity.ok().body(question);
 	}
 
+	/**
+	 *	Method Description : Get a question by id
+	 *	@param Question question
+	 *  @return Question
+	 */
 	@Override
 	public Question addQuestion(Question question) {
 		return questionRepository.save(question);
 	}
 
+	/**
+	 *	Method Description : Update a question by id
+	 *	@param Long questionId, Question question
+	 *  @return ResponseEntity<Question>
+	 */
 	@Override
 	public ResponseEntity<Question> updateQuestionById(Long questionId, Question question) throws QuestionNotFoundException {
 		Question newQuestion = questionRepository.findById(questionId).orElseThrow(() -> new QuestionNotFoundException("Question ID :: " + questionId + " Not Found"));
-		// update data here
 		newQuestion.setQuestionSentence(question.getQuestionSentence());
 		newQuestion.setQuestionChoice1(question.getQuestionChoice1());
 		newQuestion.setQuestionChoice2(question.getQuestionChoice2());
@@ -47,6 +66,11 @@ public class QuestionServiceImpl implements QuestionService{
 		return ResponseEntity.ok().body(updatedQuestion);
 	}
 
+	/**
+	 *	Method Description : Delete a question by id
+	 *	@param Long questionId
+	 *  @return ResponseEntity<Question>
+	 */
 	@Override
 	public ResponseEntity<Question> deleteQuestionById(Long questionId) throws QuestionNotFoundException {
 		Question question = questionRepository.findById(questionId).orElseThrow(() -> new QuestionNotFoundException("Question ID :: " + questionId + " Not Found"));
